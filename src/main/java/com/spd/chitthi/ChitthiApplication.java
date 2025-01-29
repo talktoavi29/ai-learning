52b3d17f2656fadf351468bf9e2baf62c2d8c610
talktoavi29 <talktoavi29@gmail.com>
2025-01-28 23:48:11 -0600

    iris ml

---
+++ src/main/java/com/spd/chitthi/ChitthiApplication.java
@@ -0,0 +1,13 @@
+package com.spd.chitthi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChitthiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChitthiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spd/chitthi/ChitthiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spd.chitthi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChitthiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
